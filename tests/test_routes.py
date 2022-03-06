f1225fa5166891dc7dcc040175fa73e50b26c729
Jahnvi Arya <ja4158@nyu.edu>
2022-03-06 15:46:14 -0500

    api to list all wishlists

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -5,6 +5,8 @@ import os
 
 from service import routes, status
 from pymodm.connection import connect
+from service.models import Item, Wishlist, DataValidationError
+
 logging.disable(logging.CRITICAL)
 
 # Get the database from the environment (12 factor)
@@ -55,3 +57,30 @@ class TestWishlistServer(TestCase):
         data = "jimmy the fish"
         resp = self.app.post("/wishlists", data = data, content_type="plain/text")
         self.assertEqual(resp.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+    def test_get_wishlist_list(self):
+        """Get a list of wishlists"""
+        init_resp = self.app.get("/wishlists/all")
+        self.assertEqual(init_resp.status_code, status.HTTP_200_OK)
+        init_data = init_resp.get_json()
+
+        wishlist_1 = Wishlist(name="fruits", customer_id="customer_a")
+        wishlist_1.save()
+        wishlist_2 = Wishlist(name="music", customer_id="customer_b")
+        wishlist_2.save()
+        resp = self.app.get("/wishlists/all")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        # logging.info(data)
+        self.assertEqual(len(data), len(init_data))
+        resp1 = self.app.get("/wishlists/all?name=fruits")
+        self.assertEqual(resp1.status_code, status.HTTP_200_OK)
+        data1 = resp1.get_json()
+        print(data1)
+        resp2 = self.app.get("/wishlists/all?customer_id=customer_a")
+        self.assertEqual(resp2.status_code, status.HTTP_200_OK)
+        data2 = resp2.get_json()
+        print(data2)
+        
+
+    
