3f80aafcf8aa6e590811a43b656a7c8da81e0364
Yanruz <98918055+Yanruz@users.noreply.github.com>
2022-03-06 18:31:13 -0500

    #5 and #8 rename and delete
    Rename logic: "foo", "foo 2" and "xxx" exist , if we rename "xxx" with "foo", the new name should be "foo 3"

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,7 +2,9 @@ from unittest import TestCase
 import logging
 from werkzeug.datastructures import MultiDict, ImmutableMultiDict
 import os
+from datetime import datetime
 
+from service.models import Item, Wishlist
 from service import routes, status
 from pymodm.connection import connect
 logging.disable(logging.CRITICAL)
@@ -21,6 +23,8 @@ class TestWishlistServer(TestCase):
     def setUp(self):
         self.app = routes.app.test_client()
         connect(DATABASE_URI.replace('wishlists','testwishlist'))
+        Wishlist.objects.all().delete()
+        Item.objects.all().delete()
 
     def test_create_wishlist(self):
         """Create a new Wishlist"""
@@ -55,3 +59,60 @@ class TestWishlistServer(TestCase):
         data = "jimmy the fish"
         resp = self.app.post("/wishlists", data = data, content_type="plain/text")
         self.assertEqual(resp.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+    
+
+    def test_update_wishlist(self):
+        """Update an existing Wishlist"""
+
+        # create wishlists "foo"
+        new_wishlist = {"name": "foo", "customer_id": "customer_1", "items": []}
+        resp = self.app.post("/wishlists", json=new_wishlist, content_type=CONTENT_TYPE_JSON)
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # create wishlist "foo 2"
+        new_wishlist = {"name": "foo 2", "customer_id": "customer_1", "items": []}
+        resp = self.app.post("/wishlists", json=new_wishlist, content_type=CONTENT_TYPE_JSON)
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # create wishlist "xxx"
+        new_wishlist = {"name": "xxx", "customer_id": "customer_1", "items": []}
+        resp = self.app.post("/wishlists", json=new_wishlist, content_type=CONTENT_TYPE_JSON)
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # rename "xxx" with "foo", the new name should be "foo 3"
+        new_wishlist = resp.get_json()
+        logging.debug(new_wishlist)
+        new_wishlist["name"] = "foo"
+
+        # update
+        resp = self.app.put(
+            "/wishlists/{}".format(new_wishlist["_id"]),
+            json=new_wishlist,
+            content_type=CONTENT_TYPE_JSON,
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_wishlist = resp.get_json()
+        self.assertEqual(updated_wishlist["name"], "foo 3")
+
+    def test_delete_wishlist(self):
+        """Delete a Wishlist"""
+
+        # create a wishlist
+        test_item = {"item_id":1, "item_name":'test', "price":100, "discount":2, "description":"test", "date_added":datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}
+        test_wishlist = {"name":"123", "customer_id":"bar", "items":[test_item]}
+        resp = self.app.post(
+            BASE_URL, json=test_wishlist, content_type=CONTENT_TYPE_JSON
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # delete the wishlist
+        new_wishlist = resp.get_json()
+        logging.debug(new_wishlist)
+        resp = self.app.delete(
+            "{0}/{1}".format(BASE_URL, new_wishlist["_id"]), content_type=CONTENT_TYPE_JSON
+        )
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(resp.data), 0)
+
+        # make sure they are deleted
+        self.assertEqual(Wishlist.find(new_wishlist["_id"]), None)
