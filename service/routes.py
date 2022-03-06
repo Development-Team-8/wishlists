1698073dd5b17e9381b16e4789c1b6002c4c45f0
Jahnvi Arya <you@example.com>
2022-03-05 23:59:13 -0500

    Api to create a wishlist

--- service/routes.py
+++ service/routes.py
@@ -1,10 +1,12 @@
-from flask import jsonify, url_for, abort
+import json
+from flask import jsonify, request, url_for, abort
 from pymongo import MongoClient
 from pymodm.connection import connect
 import os
 from datetime import datetime
 from service.models import Item
 from . import app
+
 
 
 # Get the database from the environment (12 factor)
@@ -42,4 +44,23 @@ def mongodb_status():
         version=serverStatusResult["version"],
         uptime=serverStatusResult["uptime"],
         message="MongoDB Stats"
+    )
+
+@app.route('/wishlists', methods=['POST'])
+def create_wishlist():
+    db_obj = client.wishlist_database
+    db_wishlist_obj = db_obj.wishlists
+
+    new_wishlist_name = request.form.get('name')
+    customer_id = request.form.get('customer_id')
+    newWishList = db_wishlist_obj.insert_one({'name' : new_wishlist_name,'customer_id': customer_id, 'items' : [] })
+    if newWishList.acknowledged== False:
+        return jsonify(
+            message="Internal server error"
+        ) 
+
+    return jsonify(
+        id=str(newWishList.inserted_id),
+        name=new_wishlist_name,
+        customer_id=customer_id     
     )
