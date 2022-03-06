2ce594ba8a9c1d778b1aaa5048185e6a568430e8
Jahnvi Arya <ja4158@nyu.edu>
2022-03-06 12:00:08 -0500

    Api to create a wishlist with test cases

--- service/routes.py
+++ service/routes.py
@@ -1,10 +1,11 @@
 import json
-from flask import jsonify, request, url_for, abort
+from flask import jsonify, request, url_for,json,  abort, make_response
 from pymongo import MongoClient
 from pymodm.connection import connect
 import os
 from datetime import datetime
-from service.models import Item
+from service.models import Item, Wishlist
+from service import status
 from . import app
 
 
@@ -48,19 +49,53 @@ def mongodb_status():
 
 @app.route('/wishlists', methods=['POST'])
 def create_wishlist():
+    """creates a wishlist """
     db_obj = client.wishlist_database
     db_wishlist_obj = db_obj.wishlists
 
-    new_wishlist_name = request.form.get('name')
-    customer_id = request.form.get('customer_id')
-    newWishList = db_wishlist_obj.insert_one({'name' : new_wishlist_name,'customer_id': customer_id, 'items' : [] })
-    if newWishList.acknowledged== False:
-        return jsonify(
-            message="Internal server error"
-        ) 
+    data = {}
+    # Check for form submission data
+    if request.headers.get("Content-Type") == "application/x-www-form-urlencoded":
+        app.logger.info("Getting data from form submit")
+        data = {
+            "name": request.form["name"],
+            "customer_id": request.form["customer_id"],
+            "items": [],
+        }
+    else:
+        check_content_type("application/json")
+        app.logger.info("Getting json data from API call")
+        data = request.get_json()
 
-    return jsonify(
-        id=str(newWishList.inserted_id),
-        name=new_wishlist_name,
-        customer_id=customer_id     
+    newWishList = db_wishlist_obj.insert_one(data)
+
+    location_url="location_url"
+
+    return make_response(
+        jsonify(
+            id=str(newWishList.inserted_id),
+            name=data['name'],
+            customer_id=data['customer_id']
+        ), status.HTTP_201_CREATED, {"Location": location_url}
     )
+
+
+
+def check_content_type(content_type):
+    """Checks that the media type is correct"""
+    if "Content-Type" not in request.headers:
+        app.logger.error("No Content-Type specified.")
+        abort(
+            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            "Content-Type must be {}".format(content_type),
+        )
+
+    if request.headers["Content-Type"] == content_type:
+        return
+
+    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        "Content-Type must be {}".format(content_type),
+    )
+
